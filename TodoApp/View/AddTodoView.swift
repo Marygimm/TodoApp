5f572eeab04c8e3baa089cb00ee2792e3f82af74
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-27 08:53:30 +0000

    update class iconNames

--- TodoApp/View/AddTodoView.swift
+++ TodoApp/View/AddTodoView.swift
@@ -109,31 +109,3 @@ struct AddTodoView_Previews: PreviewProvider {
       .previewDevice("iPhone 12 Pro")
   }
 }
-
-// MARK: - ALTERNATE ICONS
-
-class IconNames: ObservableObject {
-  var iconNames: [String?] = [nil]
-  @Published var currentIndex = 0
-  
-  init() {
-    getAlternateIconNames()
-    
-    if let currentIcon = UIApplication.shared.alternateIconName {
-      self.currentIndex = iconNames.firstIndex(of: currentIcon) ?? 0
-    }
-  }
-  
-  func getAlternateIconNames() {
-    if let icons = Bundle.main.object(forInfoDictionaryKey: "CFBundleIcons") as? [String: Any],
-      let alternateIcons = icons["CFBundleAlternateIcons"] as? [String: Any] {
-      for (_, value) in alternateIcons {
-        guard let iconList = value as? Dictionary<String,Any> else { return }
-        guard let iconFiles = iconList["CFBundleIconFiles"] as? [String] else { return }
-        guard let icon = iconFiles.first else { return }
-        
-        iconNames.append(icon)
-      }
-    }
-  }
-}
